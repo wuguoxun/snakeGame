5bd869bb4ce7298500f90717bce4868bed9bf583
wuguoxun <william.wu487@gmail.com>
2020-04-13 22:25:27 -0700

    FINAL

---
+++ sketch.js
@@ -0,0 +1,46 @@
+let s;
+let scl = 20;
+let food;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+}
+
+function pickLocation() {
+  let cols = floor(width / scl);
+  let rows = floor(height / scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+
+
+function draw() {
+  background(118, 153, 129);
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  s.death()
+  s.update();
+  s.show();
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
+}
